1c853bbadd67cc0aff3617a4db6c73abb28c0661
Yang Yuan <yangyuan@users.noreply.github.com>
2018-12-01 01:22:51 -0800

    xx

---
+++ App.js
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Constants } from 'expo';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+import { AuthLoadingScreen } from './screens/AuthLoadingScreen'
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>
+          Change code in23 the editor and watch it change on your phone! Save to get a shareable url.
+        </Text>
+        <Card>
+          <AssetExample />
+        </Card>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+
+// Implementation of HomeScreen, OtherScreen, SignInScreen, AuthLoadingScreen
+// goes here.
+
+const AppStack = createStackNavigator({ Home: App, Other: App });
+const AuthStack = createStackNavigator({ SignIn: App });
+
+export default createAppContainer(createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    App: AppStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  }
+));
